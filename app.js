36a9530e5adf064fd5fbe89cc053b66fb1af2570
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-06-14 08:40:12 -0400

    add(accountsRouter)

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
 const app = express();
-const PORT = '8080'
+const PORT = '8080';
 
-app.get('/', (req, res) => {
+const accountsRouter = require('./routers/accountsRouter');
+
+
+app.use('/accounts', accountsRouter);
+// app.use('/users', usersRouter);
+// app.use('/users/:id/cart', cartRouter);
+// app.use('/products', productsRouter);
+// app.use('/orders', ordersRouter);
+
+
+app.get('/test', (req, res) => {
     res.json('test ok');
 })
 
