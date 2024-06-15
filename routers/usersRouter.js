ece4bf6bacae7ed3624473d77349d8f598e99b15
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-06-15 11:46:20 -0400

    add(/register)
    adds basic form data to database, DOES NOT HASH OR SALT PASSWORD

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const usersRouter = express.Router();
+const db = require('../database/db.js');
+const multer = require('multer');
+const upload = multer();
+const dateCreated = require('../middleware/dateCreated');
 
-usersRouter.get(('/test'), (req, res)=> {
+
+const type = 'users';
+
+usersRouter.get('/test', (req, res)=> {
     res.json('users - test ok');
+})
+
+usersRouter.post('/register', upload.none(), dateCreated, async (req, res) => {
+    const model = req.body;
+    model.date_created = req.dateCreated;
+    
+    // Unique username in development
+    if (process.env.NODE_ENV == "development") model.username = Math.floor(Math.random()*100000)
+
+    const added = await db.addInstance(type, model);
+    if (!added ) return res.status(501).json('Not added to the database');
+    res.status(201).json(model);
 })
 
 module.exports = usersRouter;
