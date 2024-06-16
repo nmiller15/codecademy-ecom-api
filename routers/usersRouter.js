b6ef61a4432902df9b68bde356bec6a2fa1e763d
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-06-16 09:56:54 -0400

    add(hashPassword)
    hash submitted password before they are sent to the database

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -4,6 +4,29 @@ const db = require('../database/db.js');
 const multer = require('multer');
 const upload = multer();
 const dateCreated = require('../middleware/dateCreated');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// Password Hashing and Comparison
+
+const hashPassword = async (req, res, next) => {
+    const password = req.body.password;
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        if(err) console.error(err);
+        req.body.password = hash;
+        next(); 
+    })
+}
+
+const comparePassword = async (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const hashPassword = await db.getPassword(username);
+
+    const matches = await bcrypt.compareSync(password, hashPassword);
+
+
+}
 
 
 const type = 'users';
@@ -12,12 +35,13 @@ usersRouter.get('/test', (req, res)=> {
     res.json('users - test ok');
 })
 
-usersRouter.post('/register', upload.none(), dateCreated, async (req, res) => {
+// ! ADD SALT AND HASH FOR PASSWORD!!!
+usersRouter.post('/register', upload.none(), dateCreated, hashPassword, async (req, res) => {
     const model = req.body;
     model.date_created = req.dateCreated;
     
     // Unique username in development
-    if (process.env.NODE_ENV == "development") model.username = Math.floor(Math.random()*100000)
+    if (process.env.NODE_ENV == "development") model.username = Math.floor(Math.random()*1000000)
 
     const added = await db.addInstance(type, model);
     if (!added ) return res.status(501).json('Not added to the database');
