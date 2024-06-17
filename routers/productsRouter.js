e2b02b69348e4421b3d3a1fadde2b025afbd8c01
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-06-17 16:30:02 -0400

    add(get /products/:id)

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -8,11 +8,26 @@ productsRouter.get('/test', (req, res) => {
 
 const type = 'products';
 
+const getProductMiddleware = async (req, res, next) => {
+    const id = req.params.id;
+    const response = await db.getInstanceById(type, id);
+    if (!response) return res.status(500).send('Could not communicate with the database.');
+    req.product = response;
+    next();
+}
+
 productsRouter.get('/', async (req, res) => {
     const response = await db.getAllInstances(type);
     if (!response) return res.status(500).send('Could not access product list.')
     const products = response.rows;
-    res.json({ products });
+    res.json(products);
+})
+
+productsRouter.use('/:id', getProductMiddleware)
+
+productsRouter.get('/:id', async (req, res) => {
+    const product = req.product
+    res.json(product);
 })
 
 module.exports = productsRouter
