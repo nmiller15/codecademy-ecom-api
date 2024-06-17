a73201db5b71358fd929698b936d7a3e7b0f4d8f
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-06-17 16:21:12 -0400

    add(get /products)
    endpoint to get all products

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const productsRouter = express.Router();
+const db = require('../database/db');
 
 productsRouter.get('/test', (req, res) => {
     res.json('products - test ok')
 });
+
+const type = 'products';
+
+productsRouter.get('/', async (req, res) => {
+    const response = await db.getAllInstances(type);
+    if (!response) return res.status(500).send('Could not access product list.')
+    const products = response.rows;
+    res.json({ products });
+})
 
 module.exports = productsRouter
